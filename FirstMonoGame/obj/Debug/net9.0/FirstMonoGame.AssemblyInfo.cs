7033e5b7dd39f3ea5e38a4e4bfddf24651d39385
Michael <hubu99@gmail.com>
2026-02-13 22:18:10 -0400

    Post-Build (Added gitignore for Visual Studio)

--- FirstMonoGame/obj/Debug/net9.0/FirstMonoGame.AssemblyInfo.cs
+++ FirstMonoGame/obj/Debug/net9.0/FirstMonoGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstMonoGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f07013404d89af86689eedce7de1fc43f475b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00e1f4e96f2305dc9e138d31fea8c0c96a0ecbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstMonoGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstMonoGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
